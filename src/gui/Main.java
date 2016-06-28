970c62789cb6c86e66bab34f0393a957d595baac
Avi <aganguli@andrew.cmu.edu>
2016-06-28 00:35:14 -0400

    Bare GUI created with minimal functionality.

---
+++ src/gui/Main.java
@@ -0,0 +1,35 @@
+package gui;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+/**
+ * Main class running my start screen
+ * @author StrawHatJedi
+ *
+ */
+public class Main {
+	private static final String TITLE = "Auto-Test";
+	
+	/**
+	 * main runner
+	 * @param args to be run with (nothing)
+	 */
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				JFrame app = new JFrame(TITLE);
+				StartScreen start= new StartScreen(app);
+				start.setOpaque(true);
+				app.add(start);
+				app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				app.validate();
+				app.pack();
+				app.setVisible(true);
+				app.setResizable(true);
+			}
+		});
+	}
+
+}
