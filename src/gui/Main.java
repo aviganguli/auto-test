8fd31e941f4a930908665427d532f18a05dfb6f4
Avi <aganguli@andrew.cmu.edu>
2016-07-20 01:58:58 -0400

    Added basic key board detection functionality

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,6 +1,5 @@
 package gui;
 
-
 import java.io.IOException;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
@@ -8,41 +7,44 @@ import javax.swing.SwingUtilities;
 
 /**
  * Main class running my start screen
+ * 
  * @author StrawHatJedi
  *
  */
 public class Main {
 	private static final String TITLE = "Auto-Test";
-	private static final String MAC_SCRIPT_PATH = System.getProperty("user.dir") +
-			"/enableAssistiveApp.scpt" ;
-	
+	private static final String MAC_SCRIPT_PATH = System.getProperty("user.dir") + "/enableAssistiveApp.scpt";
+
 	/**
 	 * main runner
-	 * @param args to be run with (nothing)
+	 * 
+	 * @param args
+	 *            to be run with (nothing)
 	 */
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
-				try {
-					Runtime.getRuntime().exec("osascript " + MAC_SCRIPT_PATH);
-				} catch (IOException e) {
-					e.printStackTrace();
-					throw new IllegalStateException("Scripts failed!");
+				String osType = System.getProperty("os.name").toLowerCase();
+				if (osType.contains("mac")) {
+					try {
+						Runtime.getRuntime().exec("osascript " + MAC_SCRIPT_PATH);
+					} catch (IOException e) {
+						e.printStackTrace();
+						throw new IllegalStateException("Scripts failed!");
+					}
 				}
 				JFrame app = new JFrame(TITLE);
-				final StartScreen start= new StartScreen(app);
+				final StartScreen start = new StartScreen(app);
 				start.setOpaque(true);
 				app.add(start);
 				app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				app.validate();
 				app.pack();
 				app.setVisible(true);
-				app.setResizable(true);	
+				app.setResizable(true);
 			}
 		});
 	}
 
 }
-
-
